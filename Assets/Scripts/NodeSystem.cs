d61ab77b7c093e5df0da9400b1fc627d15339404
Boris Mezhibovskiy <borman500@msn.com>
2023-02-14 12:14:48 -0600

    improved file structure

---
+++ Assets/Scripts/NodeSystem.cs
@@ -0,0 +1,132 @@
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+public struct GridNode : IComponentData
+{
+    public float3 velocity;
+    public bool isDead;
+}
+
+public struct SectorObject : IComponentData
+{
+    public float radius;
+}
+
+public partial class NodeSystem : SystemBase
+{
+    private JobHandle updateNodeVelocities;
+    private JobHandle disposeSectorObjectsArray;
+    private JobHandle renderNodes;
+    private JobHandle updateGridNodePositions;
+    private JobHandle removeDeadNodes;
+
+    private static float3 NodeVelocityAt(float3 nodePosition, float3 sectorObjectPosition)
+    {
+        float order = 2f;
+        float perpendicularStrength = 0f;
+        float pullStrength = 0.01f;
+
+        float3 dir = sectorObjectPosition - nodePosition;
+        float3 dir2 = math.cross(dir, new float3(0, 0, 1));
+        //Inverse r squared law generalizes to inverse r^(dim-1)
+        //However, we need to multiply denom by dir.magnitude to normalize dir
+        //So that cancels with the fObj.dimension - 1, removing the - 1
+        //However #2, dir.sqrMagnitude is cheaper, but will require bringing back the - 1
+        float denom = Mathf.Pow(math.distancesq(sectorObjectPosition, nodePosition), (order - 1f));
+        return (pullStrength / denom) * dir + (perpendicularStrength / denom) * dir2;
+    }
+
+    private EndSimulationEntityCommandBufferSystem ecbSystem;
+
+    protected override void OnCreate()
+    {
+        ecbSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+    }
+
+    [BurstCompile]
+    protected override void OnUpdate()
+    {
+        //updateNodeVelocities.Complete();
+        //disposeSectorObjectsArray.Complete();
+        //renderNodes.Complete();
+        //updateGridNodePositions.Complete();
+        //removeDeadNodes.Complete();
+
+        NativeArray<Entity> sectorObjects = GetEntityQuery(typeof(SectorObject), typeof(Translation)).ToEntityArray(Allocator.TempJob);
+
+        updateNodeVelocities = Entities
+            .WithAll<GridNode>()
+            .WithReadOnly(sectorObjects)
+            .ForEach(
+            (ref Entity e, ref GridNode gridNode) =>
+            {
+                Translation translation = GetComponent<Translation>(e);
+                gridNode.velocity = float3.zero;
+                for (int i = 0; i < sectorObjects.Length; ++i)
+                {
+                    Translation soTranslation = GetComponent<Translation>(sectorObjects[i]);
+                    SectorObject soComponent = GetComponent<SectorObject>(sectorObjects[i]);
+                    float distSq = math.distancesq(translation.Value, soTranslation.Value);
+                    if (distSq < soComponent.radius * soComponent.radius)
+                    {
+                        gridNode.isDead = true;
+                    }
+                    else
+                    {
+                        gridNode.velocity += NodeVelocityAt(translation.Value, soTranslation.Value);
+                    }
+                }
+            }
+        ).ScheduleParallel(Dependency);
+        Dependency = updateNodeVelocities;
+
+        disposeSectorObjectsArray = sectorObjects.Dispose(updateNodeVelocities);
+        Dependency = disposeSectorObjectsArray;
+
+        renderNodes = Entities
+            .WithAll<GridNode, Translation>()
+            .ForEach(
+            (in GridNode gridNode, in Translation translation) =>
+            {
+                float3 velVec = gridNode.velocity;
+                if (math.distancesq(velVec, float3.zero) < 0.00001f)
+                {
+                    velVec = new float3(1, 0, 0) * 0.001f;
+                }
+                Debug.DrawRay(translation.Value, velVec * 20f);
+            }
+        ).ScheduleParallel(disposeSectorObjectsArray);
+        Dependency = renderNodes;
+
+        updateGridNodePositions = Entities
+            .WithAll<Translation, GridNode>()
+            .ForEach(
+            (ref Translation translation, in Entity e, in GridNode gridNode) =>
+            {
+                translation.Value += gridNode.velocity;
+            }
+        ).ScheduleParallel(renderNodes);
+        Dependency = updateGridNodePositions;
+
+        EntityCommandBuffer.ParallelWriter ecb = ecbSystem.CreateCommandBuffer().AsParallelWriter();
+        removeDeadNodes = Entities
+            .WithAll<GridNode>()
+            .ForEach(
+            (in Entity e, in int entityInQueryIndex, in GridNode gridNode) =>
+            {
+                if (gridNode.isDead)
+                {
+                    ecb.DestroyEntity(entityInQueryIndex, e);
+                }
+            }
+        ).ScheduleParallel(updateGridNodePositions);
+        ecbSystem.AddJobHandleForProducer(removeDeadNodes);
+        Dependency = removeDeadNodes;
+    }
+
+}
---
+++ Assets/Scripts/ShipSystem.cs
@@ -0,0 +1,136 @@
+using System.Collections.Generic;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+public struct ClosestNodes
+{
+    [ReadOnly] public const int numClosestNodes = 3;
+    public Entity closestNode1;
+    public Entity closestNode2;
+    public Entity closestNode3;
+    public Entity Get(int index)
+    {
+        switch (index)
+        {
+            case 0:
+                return closestNode1;
+            case 1:
+                return closestNode2;
+            case 2:
+                return closestNode3;
+            default:
+                return Entity.Null;
+        }
+    }
+
+    public void Set(Entity e, int index)
+    {
+        switch (index)
+        {
+            case 0:
+                closestNode3 = closestNode2;
+                closestNode2 = closestNode1;
+                closestNode1 = e;
+                break;
+            case 1:
+                closestNode3 = closestNode2;
+                closestNode2 = e;
+                break;
+            case 2:
+                closestNode3 = e;
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+public struct Ship : IComponentData
+{
+    public ClosestNodes closestNodes;
+    public float3 nodeOffset;
+}
+
+public partial class ShipSystem : SystemBase
+{
+    private JobHandle updateShips;
+    private JobHandle disposeNodesArray;
+
+    [BurstCompile]
+    protected override void OnUpdate()
+    {
+        ComponentDataFromEntity<Translation> translationData = GetComponentDataFromEntity<Translation>();
+
+        NativeArray<Entity> nodes = GetEntityQuery(typeof(GridNode), typeof(Translation)).ToEntityArray(Allocator.TempJob);
+
+        updateShips = Entities
+            .WithAll<Ship, Translation>()
+            .WithReadOnly(nodes)
+            .WithReadOnly(translationData)
+            .ForEach(
+            (ref Ship s, in Translation t) =>
+            {
+                //Instead of just sorting the nodes array,
+                //It should be faster to just find the closest K nodes (currently 3)
+                //So, this algorithm has K*N iterations, where N is the total number of nodes
+                //Since K is very small, this has a O(N).
+                //Also, it doesn't require copying an entire array to sort it.
+                float3 shipPos = t.Value;
+                s.closestNodes = new ClosestNodes { closestNode1 = Entity.Null, closestNode2 = Entity.Null, closestNode3 = Entity.Null };
+                for (int i = 0; i < nodes.Length; ++i)
+                {
+                    float3 nodePos = translationData[nodes[i]].Value;
+                    float newSqMag = math.distancesq(nodePos, shipPos);
+
+                    for (int j = 0; j < ClosestNodes.numClosestNodes; ++j)
+                    {
+                        Entity currentClosest = s.closestNodes.Get(j);
+                        if (!translationData.HasComponent(currentClosest))
+                        {
+                            s.closestNodes.Set(nodes[i], j);
+                            break;
+                        }
+
+                        float3 currentPos = translationData[currentClosest].Value;
+                        float currentSqMag = math.distancesq(currentPos, shipPos);
+                        if (newSqMag < currentSqMag)
+                        {
+                            s.closestNodes.Set(nodes[i], j);
+                            break;
+                        }
+                    }
+                }
+
+                //Once we've updated the closest nodes, we can draw lines for debug visualization
+                for (int i = 0; i < ClosestNodes.numClosestNodes; ++i)
+                {
+                    float3 nodePos = translationData[s.closestNodes.Get(i)].Value;
+                    Debug.DrawLine(shipPos, nodePos);
+                }
+            }
+            ).ScheduleParallel(Dependency);
+        Dependency = updateShips;
+
+        disposeNodesArray = nodes.Dispose(updateShips);
+        Dependency = disposeNodesArray;
+    }
+}
+public struct EntityComparer : IComparer<Entity>
+{
+    [ReadOnly] public float3 pos;
+    [ReadOnly] public ComponentDataFromEntity<Translation> translationData;
+
+    public int Compare(Entity x, Entity y)
+    {
+        float3 fX = translationData[x].Value;
+        float3 fY = translationData[y].Value;
+        float sqDistX = math.distancesq(fX, pos);
+        float sqDistY = math.distancesq(fY, pos);
+        return sqDistX.CompareTo(sqDistY);
+    }
+}
